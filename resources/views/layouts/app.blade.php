77cf88bcc66074b8a4b6c4c6cee79242457a6e0a
Kishi79 <bimosatryo123@gmail.com>
2025-09-07 19:16:14 +0700

    updte logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,11 +37,8 @@
                         <!-- Logo -->
                         <div class="shrink-0 flex items-center">
                             <a href="{{ url('/') }}" class="flex items-center">
-                                <svg class="w-8 h-8 text-blue-600 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
-                                          d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"/>
-                                </svg>
-                                <span class="text-xl font-semibold">Satu Data Garut</span>
+                                <img class="h-10" src="{{ asset('https://satudata.garutkab.go.id/_next/image/?url=%2Fassets%2Fimages%2Flogo-satudata.png&w=256&q=75') }}" alt="Logo">
+                                <span class="text-xl font-semibold"></span>
                             </a>
                         </div>
 
